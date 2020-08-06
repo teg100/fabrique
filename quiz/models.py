25a7582b1b623fb9cadb1eb20d5194ab48ffda0f
Vladimir Chuikov <python46dev@gmail.com>
2020-08-06 13:53:28 +0400

    Добавляет модель Question

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+
+    TEXT = 'text'
+    RADIO = 'radio'
+    SELECT = 'multiple'
+
+    QUESTION_TYPE = (
+        (TEXT, 'text'),
+        (RADIO, 'one answer'),
+        (SELECT, 'multi answer')
+    )
+
+    text = models.CharField(max_length=512)
+    type = models.CharField(max_length=40, choices=QUESTION_TYPE)
+    choises = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.text
