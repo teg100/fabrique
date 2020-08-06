b43c5cb6c3f7e1c4ecf6645621b2ca291aac8b30
Vladimir Chuikov <python46dev@gmail.com>
2020-08-06 14:14:14 +0400

    Добавляет serializers.py и serializers для моделей.

---
+++ quiz/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import *
+
+
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['url', 'id', 'answer', 'type', 'choises']
+
+
+class QuizSerializers(serializers.HyperlinkedModelSerializer):
+
+    questions = QuestionSerializer(many=True)
+
+    class Meta:
+        model = Quiz
+        fields = ['url', 'id', 'title', 'start_date', 'end_date', 'description',
+                  'is_active', 'questions']
+
+class AnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Answer
+        fields = ['id', 'question', 'text']
+
+
+class ResponseSerializer(serializers.ModelSerializer):
+
+    answers = AnswerSerializer(many=True)
+
+    class Meta:
+        model = Response
+        fields = ['id', 'uid', 'quiz', 'answers']
+
