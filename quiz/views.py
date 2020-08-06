a12354fecfe3bea2cc7fc5d6d5ecef616298e2a9
Vladimir Chuikov <python46dev@gmail.com>
2020-08-06 14:22:12 +0400

    Добавляет viewset для Quiz

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+class QuizViewSet(viewsets.ModelViewSet):
+    serializer_class = QuestionSerializer
+
+    def get_queryset(self):
+        if self.action == 'list':
+            if not self.request.user.is_superuser:
+                return Quiz.objects.filter(is_active=True)
+        return Quiz.objects.all()
