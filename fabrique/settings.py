b507d5c562262b413d31f4c52891b69b40edda00
Vladimir Chuikov <python46dev@gmail.com>
2020-08-07 19:25:57 +0400

    Добавляет авторизацию по токену

--- fabrique/settings.py
+++ fabrique/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'quiz'
 ]
 
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
